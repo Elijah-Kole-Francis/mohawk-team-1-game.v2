3668445bb53c58c9333805a5aae4ee732f6c4b3b
elija <elija@LOUBITAN>
2024-11-12 19:35:05 -0500

    Initial group commit

---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.0)1/Game.cs
@@ -0,0 +1,32 @@
+﻿// Include code libraries you need here:
+using System;
+using System.Numerics;
+
+// Where your code is placed.
+namespace GAME_10003_Game_Development_Foundations___2D_Game_Template__v1._0_1
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+}
