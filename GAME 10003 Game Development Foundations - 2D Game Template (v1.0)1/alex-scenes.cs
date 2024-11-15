fe3e67196ca01a1a1e73092a36bd12cf57d34a98
elija <elija@LOUBITAN>
2024-11-14 20:52:01 -0500

    add classes

---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.0)1/alex-scenes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_10003_Game_Development_Foundations___2D_Game_Template__v1._0_1
+{
+    public class scenes
+    {
+
+    }
+}
