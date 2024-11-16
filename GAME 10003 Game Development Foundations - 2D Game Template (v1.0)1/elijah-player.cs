363dab21d92aff73eb5438d4c450cae5952791f8
elija <elija@LOUBITAN>
2024-11-16 17:54:41 -0500

    add damage, health, and spell logic

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.0)1/elijah-player.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.0)1/elijah-player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,36 @@ namespace GAME_10003_Game_Development_Foundations___2D_Game_Template__v1._0_1
 {
     public class player
     {
+        private float currentHealth;
+        private float damage;
+        private Vector2 playerHealth;
+        private float healthRegen;
+        private float spellcost;
+        private float mana;
+        private float enemyHealth;
+        public player()
+        {
 
+        }
+
+        private void TakingDamage(float damage)
+        {
+            currentHealth -= damage;
+        }
+
+        private void HealthReven()
+        {
+            healthRegen += currentHealth;
+        }
+
+        private void DealingDamage(float damage)
+        {
+            enemyHealth -= damage;
+        }
+
+        private void Spellcasting(float spellcost)
+        {
+            mana -= spellcost;
+        }
     }
 }
